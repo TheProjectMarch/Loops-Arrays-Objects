dc2c425b3d8fd0f66219f34fa2798d83213d10b3
Doug Osborn <theprojectmarch@gmail.com>
2019-01-21 00:12:11 -0500

    tinkered LOL. Learned about awesome new concat delimiter and lack of whitespace requierment to make strings readable. LOL^2

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("Hello, world!")
+var user = "Doug";
+var counter = 0;
+alert("Hello, world!");
+
+
+if (user === "Doug"){
+    alert("Hey, Doug!");
+} else {
+    alert("Uh... what do YOU want?");
+}
+
+while (counter < 4){
+    console.log(counter);
+    counter++
+}
+console.log("The counter is now at",counter,"so we're done!");
+
+alert("Goodbye!");
+
