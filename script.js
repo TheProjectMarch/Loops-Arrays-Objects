1f74785231effac10c7ebcc093a1367013ead852
Doug Osborn <theprojectmarch@gmail.com>
2019-01-21 00:49:43 -0500

    change index.html to reflect current exercise and do exercise in script.js with a guessing game while loop. lol.

--- script.js
+++ script.js
@@ -2,12 +2,24 @@
 function randomNumber(upper) {
     return Math.floor( Math.random() * upper ) + 1;
 }
-var counter = 0;
+
+//WHILE LOOP STUFF
+/* var counter = 0;
 while (counter < 100) {
     var randNum = randomNumber(6);
     document.write(randNum," ");
     counter++;
+} */
+
+//IF LOOP STUFF
+var randNum = randomNumber(23000);
+var guessCounter = 0
+while (randNum !== randNum2){
+    var randNum2 = randomNumber(23000);
+    guessCounter++
 }
+document.write("<p>The Random Number was: ",randNum,"</p>")
+document.write("<p>It took the computer ",guessCounter," attempts to get it right</p>")
 
 // muh tinkerin'. 
 /* var user = "Doug";
