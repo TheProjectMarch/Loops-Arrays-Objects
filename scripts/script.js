520bcff090f03e46bdbb0d46cdd0dc7e7b52e429
Doug Osborn <theprojectmarch@gmail.com>
2019-05-04 03:29:57 -0400

    Array Methods and shit LOL

--- scripts/script.js
+++ scripts/script.js
@@ -101,7 +101,7 @@ groceries.push('butter');
 console.log(groceries); */
 
 
-function printList(list){
+/* function printList(list){
     var listHTML = '<ol>';
     for (var i = 0;i < list.length; i+= 1){
         listHTML += '<li>' + list[i] + '</li>';
@@ -136,4 +136,145 @@ cereals.forEach((v, i, a) => {console.log(v, i,);});
 
 printList(cereals);
 
-console.log('The length of the cereals array is', cereals.length);
+console.log('The length of the cereals array is', cereals.length); */
+
+
+/* +++++++++++++++++++++++++++++++++++
+FOR LOOPS WITH ARRAYS
++++++++++++++++++++++++++++++++++++ */
+
+/* var cereals = ['Captain Crunch', 'Trix', 'Cheerios', 'Shredded Wheat', 'Special K'];
+
+for (var i = 0; i < cereals.length; i++){
+    console.log(i, cereals[i]);
+}
+
+console.log('Done!');
+
+console.log(cereals);
+console.dir(cereals);
+console.log(JSON.parse(JSON.stringify(cereals))); */
+
+
+
+/* 
++++++++++++++++++++++++++++++++
+COMPILING A PLAYLIST FROM AN ARRAY AND PRINTING IT TO A WEBPAGE VIA AN ORDERED LIST
++++++++++++++++++++++++++++++++
+
+BONUS: I DID IT WITH LITERAL TEMPLATES
+*/
+/* var playlistItems;
+
+var playlist = ['I Did it my way',
+    'Mouth for War',
+    'The Wooden Song',
+    'Counterfeit',
+    'Strange Fruit'
+];
+function print(msg){
+    document.write(msg);
+}
+function printList(list){
+    playlistItems = ``
+    for(let i = 0; i < list.length; i++){
+        playlistItems += `<li>${list[i]}</li>`;
+    }
+    var playlistHtml = `<ol>${playlistItems}</ol>`;
+    
+
+    print(playlistHtml);
+}
+
+printList(playlist); */
+
+/* 
+++++++++++++++++++++++++++++++
+USEFUL ARRAY METHODS
+++++++++++++++++++++++++++++++
+ */
+
+//JOIN - TAKES ALL THE ITEMS OF AN ARRAY AND PUTS THEM INTO A STRING AND SEPARATES EACH VALUE BY A CHOSEN DELIMITER. 
+
+/* function print(msg){
+    document.write(msg);
+}
+var cereals = ['Captain Crunch',
+    'Trix',
+    'Cheerios',
+    'Shredded Wheat',
+    'Special K'
+];
+
+var cerealString = cereals.join(" _|_ ");
+
+console.log(cerealString);
+print(cerealString); */
+
+//CONCAT - JOIN TWO OR MORE ARRAYS. THIS GIVES A NEW ARRAY. DOES NOT FUCK WITH THE ORIGINALS
+
+/* var sugarCereals = ['Trix', 'Fruity Pebbles', 'Captain Crunch'];
+var healthyCereals = ['Special K', 'Cheerios', 'Grape Nuts'];
+var allCereals;
+function print(msg){
+    document.write(msg);
+    
+}
+function printList(list, heading){
+    var listItems = ``
+    for(let i = 0; i < list.length; i++){
+        listItems += `<li>${list[i]}</li>`;
+    }
+    var listHeading = `<h2 style="padding-bottom: 10px;">${heading}</h2>`
+    var listHtml = `${listHeading}<ol>${listItems}</ol><br><br>`;
+    
+
+    print(listHtml);
+}
+console.dir(sugarCereals);
+console.dir(healthyCereals);
+
+allCereals = sugarCereals.concat(healthyCereals);
+
+console.dir(allCereals);
+
+printList(sugarCereals, "Sugar Cereals");
+printList(healthyCereals, "Healthy Cereals");
+printList(allCereals, "All Cereals");
+ */
+
+ /* 
+ ============================
+ INDEXOF - LETS YOU SEARCH AN ARRAY FOR A PARTICULAR VALUE. IF THE VALUE IS IN THE ARRAY, THEN THAT ELEMENT'S POSITION (INDEX) IS RETURNED
+ ============================
+ */
+
+
+var sugarCereals = ['Trix', 'Fruity Pebbles', 'Captain Crunch'];
+var healthyCereals = ['Special K', 'Cheerios', 'Grape Nuts'];
+var allCereals;
+var userAnswer = "";
+var question = 0;
+allCereals = sugarCereals.concat(healthyCereals);
+//console.log("Fruity Pebbles was found at Index", sugarCereals.indexOf('Fruity Pebbles'));
+
+
+while(question < 2){
+    question++
+    userAnswer = allCereals.indexOf(prompt("What cereal do you like?"))
+
+    console.log(userAnswer);
+    
+
+   /*  if (!userAnswer){
+        alert(`Hmm, not a fan of that. What else do you like?`);
+    } else {
+        alert(`Awesome! I love ${allCereals[userAnswer]}. What else do you like?`);
+    }
+ */
+}
+
+
+
+
+
